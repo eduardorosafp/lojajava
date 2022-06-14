e35f654335c58f3a6906786523e7555bb4a15bbd
eduardo rosa <eduardorosafp@gmail.com>
2022-06-14 03:46:20 -0300

    erro

--- src/main/java/com/loja/services/CategoriaService.java
+++ src/main/java/com/loja/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.loja.domain.Categoria;
 import com.loja.repositories.CategoriaRepository;
+import com.loja.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,6 @@ public class CategoriaService {
 
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
